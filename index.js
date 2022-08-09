fe6eb8ec17c8036f1b82cd9a838d007fe7b10e45
Issam Blcayd <109247531+GUNTER404@users.noreply.github.com>
2022-08-09 16:49:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import fetch from 'node-fetch'
+import { setInterval } from 'timers'
+
+const app = express()
+app.get('/',async(req,res)=>{
+  if (req.query.url){
+      re = await fetch(req.query.url)
+    try{
+      res.json(await re.json())
+    }catch{
+      res.send(await re.text())
+    }
+  }else{
+    let er = {
+  status: false
+}
+    res.json(er)
+  }
+})
+setInterval(async()=>{
+  await fetch(`https://${process.env['app']}.herokuapp.com`)
+})
+app.listen()
